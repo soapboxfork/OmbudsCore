63dbd010f8d48eccf7106c413303c2785adf153d
Nick Skelsey <nskelsey@gmail.com>
2015-05-29 16:18:29 -0400

    Added a preview bulletin and specced out the Send pane.
    The styles on buttons could use some work and I am not totally happy
    with the spacing, but it is close. Check and Tweet still need to be
    implemented.

--- src/frontend/send/mod.js
+++ src/frontend/send/mod.js
@@ -1,35 +1,57 @@
 'use strict';
 
-angular.module('sendModule', ['backendHooks', 'walletModule'])
-.controller('sendPaneCtrl', function($scope, ombWebSocket, walletService, draftService) {
+angular.module('sendModule', ['backendHooks', 'walletModule', 'browseModule'])
+.controller('sendPaneCtrl', function($scope, ombWebSocket, walletService, draftService, paneState, markdownService) {
 
     $scope.draftBltn = draftService;
     $scope.wallet = walletService;
+    $scope.state = paneState;
+    
+    // For the rendered Bltn
+    addCommonFunctions($scope, markdownService);
+    //$scope.depthImg = function(bltn) { return "" };
+    $scope.inAuthor = true;
+    $scope.testBltn = {};
 
     function prec(i) {
         return i.toFixed(3);
     }
 
     var txFee = 50000;
-    var estimate = { 'val': '0.000', 'len': 0 };
-    $scope.estimate = estimate;
 
-    $scope.updateEst = function() {
+    function updateEst(estimate) {
         estimate.len = draftService.board.length + draftService.msg.length;
         estimate.rawval = (txFee + Math.ceil(estimate.len/20)*567)*walletService.unitPerSat;
         console.log(estimate.rawval);
         estimate.val = estimate.rawval.toFixed(4);
     };
 
-    $scope.updateEst();
+    $scope.updateEst = updateEst;
 
+    updateEst(paneState.estimate);
+
+    $scope.toggleRender = function() {
+        paneState.renderTog = !paneState.renderTog;
+        if (paneState.renderTog) {
+            // Create a new bltn and load it into scope.            
+            var now = new Date().getTime();
+            var testBltn = {
+                msg: draftService.msg,
+                board: draftService.board,
+                timestamp: now,
+                author: walletService.address
+            };
+            paneState.testBltn = testBltn;
+            updateEstimate(paneState.estimate);
+        } 
+    }
 
     $scope.handleSendBltn = function() {
         
         // TODO make into a promise
-        ombWebSocket.sendBulletin(draftBltn);
-        draftBltn.msg = '';
-        draftBltn.board = '';
+        ombWebSocket.sendBulletin(draftService);
+        draftService.msg = '';
+        draftService.board = '';
     }
 })
 .factory('draftService', function() {
@@ -39,4 +61,14 @@ angular.module('sendModule', ['backendHooks', 'walletModule'])
         msg: ''
     };
     return draftBltn;
+})
+.factory('paneState', function() {
+// Holds the state of the pane across initializations
+//
+    var state = {
+        renderTog: false, // The toggle for the bltn renderer
+        testBltn: {},
+        estimate: { 'val': '0.000', 'len': 0 },
+    }
+    return state;
 });
